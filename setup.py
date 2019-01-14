7af01fafa4ed45e30b4462c85e121644fe1369df
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2019-01-14 11:42:51 +0100

    version 0.1.2

--- setup.py
+++ setup.py
@@ -2,16 +2,21 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
   name = 'eth_event',
   packages = find_packages(),
-  version = '0.1.1',
+  version = '0.1.2',
   license = 'MIT',
   description = 'Ethereum event decoder and topic generator',
+  long_description =  long_description,
+  long_description_content_type="text/markdown",
   author = 'Benjamin Hauser',
   author_email = 'ben.hauser@hyperlink.capital',
   url = 'https://github.com/iamdefinitelyahuman/eth-event',
-  download_url = 'https://github.com/iamdefinitelyahuman/eth-event/archive/0.1.1.tar.gz',
+  download_url = 'https://github.com/iamdefinitelyahuman/eth-event/archive/0.1.2.tar.gz',
   keywords = ['ethereum'],
   install_requires=[
           'eth-abi',
