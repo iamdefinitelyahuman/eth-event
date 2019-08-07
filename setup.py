26ef70a38661219c5d07f654813a74d3f5ad50f6
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-08-07 13:11:35 +0800

    bump version, changelog, readme

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 setup(
     name='eth_event',
     packages=find_packages(),
-    version='0.1.4',
+    version='0.2.0',
     license='MIT',
     description='Ethereum event decoder and topic generator',
     long_description=long_description,
