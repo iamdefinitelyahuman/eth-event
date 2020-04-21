5abf8b198040295f9c669dcfd2a9d801c4c68c90
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-04-21 21:18:29 +0400

    refactor: update internal methods

--- eth_event/main.py
+++ eth_event/main.py
@@ -244,29 +244,33 @@ def decode_traceTransaction(
     return events
 
 
-def _params(abi_params):
+def _params(abi_params: List) -> List:
     types = []
     for i in abi_params:
         if i["type"] != "tuple":
             types.append(i["type"])
             continue
         types.append(f"({','.join(x for x in _params(i['components']))})")
+
     return types
 
 
-def _decode(inputs, topics, data):
+def _decode(inputs: List, topics: List, data: str) -> List:
     try:
-        types = _params(i for i in inputs if not i["indexed"])
+        types = _params([i for i in inputs if not i["indexed"]])
     except (KeyError, TypeError):
         raise ABIError("Invalid ABI")
+
     if types and data == "0x":
         data += "0" * (len(types) * 64)
+
     try:
         decoded = list(decode_abi(types, HexBytes(data)))[::-1]
     except InsufficientDataBytes:
         raise EventError("Insufficient event data")
     except OverflowError:
         raise EventError("Cannot decode event due to overflow error")
+
     topics = topics[::-1]
     result = []
     for i in inputs:
@@ -274,25 +278,18 @@ def _decode(inputs, topics, data):
         if "components" in i:
             result[-1]["components"] = i["components"]
         if i["indexed"]:
-            try:
-                value = HexBytes(topics.pop())
-            except IndexError:
+            if not topics:
                 raise EventError("Insufficient event data")
+            encoded = HexBytes(topics.pop())
             try:
-                value = decode_single(i["type"], HexBytes(value))
+                value = decode_single(i["type"], encoded)
             except (InsufficientDataBytes, OverflowError):
-                result[-1].update({"value": value.hex(), "decoded": False})
+                result[-1].update({"value": encoded.hex(), "decoded": False})
                 continue
         else:
             value = decoded.pop()
-        value = _bytes_to_hex_string(value)
+        if isinstance(value, bytes):
+            value = HexBytes(value).hex()
         result[-1].update({"value": value, "decoded": True})
+
     return result
-
-
-def _bytes_to_hex_string(value):
-    if isinstance(value, bytes):
-        value = value.hex()
-        if not value.startswith("0x"):
-            value = "0x" + value
-    return value
