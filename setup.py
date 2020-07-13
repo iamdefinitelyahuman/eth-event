398844f2280d9328b26ad06c1d5fdaa9a6cb8fb3
Ben Hauser <ben@hauser.id>
2020-07-14 01:20:23 +0300

    dep: add eth_utils as dependency

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     install_requires=[
         "eth-abi>=2.0.0,<3.0.0",
         "eth-hash[pycryptodome]>=0.2.0,<1.0.0",
+        "eth-utils>=1.2.0,<2.0.0",
         "hexbytes>=0.2.0,<1.0.0",
     ],
     classifiers=[
