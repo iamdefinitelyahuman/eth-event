25d472ae0462c9269084f5ed794e37237b13a622
Ben Hauser <ben@hauser.id>
2024-02-02 03:46:51 +0400

    chore: loosen pins, regress hexbytes dependency to <1

--- setup.py
+++ setup.py
@@ -23,10 +23,10 @@ setup(
     packages=find_packages(exclude=["tests", "tests.*"]),
     python_requires=">=3.6, <4",
     install_requires=[
-        "eth-abi>=5,<6",
+        "eth-abi>=4,<6",
         "eth-hash[pycryptodome]>=0.2.0,<1.0.0",
-        "eth-utils>=3,<4",
-        "hexbytes>=1,<2",
+        "eth-utils>=2,<4",
+        "hexbytes<1",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
