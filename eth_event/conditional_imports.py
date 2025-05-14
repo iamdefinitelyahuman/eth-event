8be1bf11527343e1d6283888bc2117e96206ab71
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-14 19:41:35 -0400

    Create conditional_imports.py

---
+++ eth_event/conditional_imports.py
@@ -0,0 +1,8 @@
+from typing import Type
+
+
+try:
+    from eth_abi.exceptions import InvalidPointer
+except ImportError:
+    # Define a stub exception for older eth-abi versions
+    InvalidPointer: Type[Exception] = type("InvalidPointer", (Exception,), {})
