8b7172d7964685039bc57a708723c6c38a35bffb
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-30 08:27:42 -0400

    classifiers

--- setup.py
+++ setup.py
@@ -58,5 +58,10 @@ setup(
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 )
